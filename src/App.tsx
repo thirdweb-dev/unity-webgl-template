7b10f7a1e8007c7c8ba128099f454773abeaf53f
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2021-11-16 02:23:26 -0800

    unity-bridge

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,60 @@
-import { ConnectWallet } from "@3rdweb/react";
-import { useEffect, useRef } from "react";
-import { useUnity } from "./gateway/unity";
+import { ThirdwebBridgeSDK } from "@3rdweb/unity-bridge";
+import { ethers } from "ethers";
+import { Magic } from "magic-sdk";
+import { useCallback, useEffect, useRef } from "react";
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  useUnity();
-  useEffect(() => {
 
-    if(!canvasRef.current) {
+  // Initialize ThirdwebBridgeSDK with an RPC url
+  const bridge = useRef<ThirdwebBridgeSDK>(
+    new ThirdwebBridgeSDK("https://rpc-mumbai.maticvigil.com")
+  );
+
+  // Setting up Unity bridge instance
+  useEffect(() => {
+    if (!canvasRef.current) {
       return;
     }
 
     const interval = setInterval(() => {
-      if(!(window as any).createUnityInstance) {
+      if (!(window as any).createUnityInstance) {
         return;
       }
-
       clearInterval(interval);
-      (window as any).createUnityInstance(canvasRef.current, (window as any).unityConfig).then((unityInstance: any) => {
-        (window as any).unityInstance = unityInstance;
-      });
+      (window as any)
+        .createUnityInstance(canvasRef.current, (window as any).unityConfig)
+        .then((unityInstance: any) => {
+          (window as any).unityInstance = unityInstance;
+        });
     }, 500);
   }, [canvasRef]);
+
+  // Connect to magic.link or Metamask and setup ThirdwebSDK.
+  const onClickMagicLogin = useCallback(async () => {
+    // login with email (magic.link)
+    const magic = new Magic("<MAGIC_PUBLISHABLE_KEY>", {
+      network: {
+        chainId: 80001,
+        rpcUrl: "https://rpc-mumbai.maticvigil.com",
+      },
+    });
+    await magic.auth.loginWithMagicLink({ email: "example@email.com" });
+
+    // update the signer on thirdweb sdk
+    const provider = new ethers.providers.Web3Provider(
+      magic.rpcProvider as any
+    );
+    const signer = provider.getSigner();
+    // bridge.current because of useRef
+    bridge.current.setProviderOrSigner(signer);
+
+    console.log("logged in address", await signer.getAddress());
+  }, []);
+
   return (
     <div>
-      hello
+      <button onClick={onClickMagicLogin}>Connect with magic.link</button>
       <canvas
         ref={canvasRef}
         id="unity-canvas"
