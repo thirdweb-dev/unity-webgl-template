71a4ceadc1eca26c90bc87b3c5abf25bbc81f5ad
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2022-01-17 23:34:14 +0000

    upgrade packages

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   style: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
+    postcssOptions: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
-}
+};
+
